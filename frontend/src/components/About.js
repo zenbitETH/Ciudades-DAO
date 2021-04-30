b5ccb9966bb16d201fddaa824c74a3a6d9fc16fb
HabacucV <habacuc.vera@gmail.com>
2021-04-29 19:13:42 -0500

    css

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -4,20 +4,19 @@ import {Button} from 'react-bootstrap';
 const About = () => {
   return (
     <div className="App">
-      <div className="gray">
-      <div>
-        this page will 
-        1. describe what TARO is, 
-        2. explain the proposal system
-        3. walk users through the process of getting metamask,connecting to metamask and getting validated the link to install metamask
-        https://metamask.io/download.html
-      </div>
+      <div className="gray3">
         <div>
-        The last step for the user to begin earning TARO is to validate their account.  This is done by taking a quiz about Queretaro.  This helps to keep the TARO tokens among people who care about their city.
-       </div>
-        <Button  >Take the Queretaro quiz</Button>
-        <Link to="/quiz"></Link>
+          this page will 
+          1. describe what TARO is, 
+          2. explain the proposal system
+          3. walk users through the process of getting metamask,connecting to metamask and getting validated the link to install metamask
+          https://metamask.io/download.html
+        </div>
+        <div>
+          The last step for the user to begin earning TARO is to validate their account.  This is done by taking a quiz about Queretaro.  This helps to keep the TARO tokens among people who care about their city.
+        </div>
       </div>
+      <Button className="Wallet" href="/quiz" >Take the Queretaro quiz</Button>
     </div>
   );
 };
