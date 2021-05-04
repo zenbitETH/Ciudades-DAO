4bf320d46d9b7dbc95f1d79c2ab15fadb966fc33
HabacucV <habacuc.vera@gmail.com>
2021-05-04 11:26:37 -0500

    Import style from home.css

--- frontend/src/components/buttons/ConnectingButton.js
+++ frontend/src/components/buttons/ConnectingButton.js
@@ -1,7 +1,7 @@
 import { Button, Spinner } from 'react-bootstrap';
 import { useContext } from 'react';
 import { LanguageContext } from '../../contexts/LanguageContext';
-import '../styles/Home.css';
+import '../../styles/Home.css';
 
 
 const ConnectingButton = () => {
