53bebe8e8e04a76b7b2bdac8209ac978dc3d8b27
HabacucV <habacuc.vera@gmail.com>
2021-05-04 13:51:41 -0500

    Connect wallet and Navbar updates

--- frontend/src/components/buttons/ConnectButtonEng.js
+++
@@ -1,9 +0,0 @@
-import { Button } from 'react-bootstrap';
-
-const ConnectButton = ({handleOnConnect}) => {
-  return (
-    <Button onClick={handleOnConnect}>Connect Wallet</Button>
-  );
-};
-
-export default ConnectButton;
