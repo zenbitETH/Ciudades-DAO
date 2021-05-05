8f7a14d68d9f3a53707e51422934522b70b87888
HabacucV <habacuc.vera@gmail.com>
2021-05-04 19:19:27 -0500

    Logo and about update

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -84,8 +84,8 @@ const About = () => {
       <Button  a className="alt" href="https://metamask.io" >Descargar Cartera</Button>
       <div className="main">
         <p>2. El token TARO solo se puede obtener como recompensa 🥇 al:
-        <p className="main2">✔️ Verificar que eres ciudadan@ queretan@ al final de esta pantalla.
-        <p className="main2">🗳️ Participar en la gobernanza de la ciudad.</p></p></p>
+        <p className="main2">✔️ Verificar que eres ciudadan@ queretan@ al final de esta pantalla.</p>
+        <p className="main2">🗳️ Participar en la gobernanza de la ciudad.</p></p>
       </div>
       <div className="orange">5. Beneficios para la ciudad</div>
       <div className="text-medium-left">
