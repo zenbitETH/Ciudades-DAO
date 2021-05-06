2147e4c7e5e6c2919f7313bba9f16d79191785db
HabacucV <habacuc.vera@gmail.com>
2021-05-06 10:04:41 -0500

    CreateProposal css

--- frontend/src/components/CreateProposal.js
+++ frontend/src/components/CreateProposal.js
@@ -133,7 +133,7 @@ let {isEnglish} = useContext(LanguageContext);
         </Form.Label>        
           <Form.Control type="text" placeholder="required TARO to vote" onChange={handleOnChangeRequiredTaroToVote}/>          
         </Form.Group>
-        <Button as={Row} classNtype="submit" onClick={handleOnSubmit}>Submit proposal</Button>
+        <Button className-="submit"classNtype="submit" onClick={handleOnSubmit}>Submit proposal</Button>
       </Form>
 
       <IsLoadingModal
