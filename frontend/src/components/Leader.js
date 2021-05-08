634d77df50822485a8b0532c7966504279dd397d
HabacucV <habacuc.vera@gmail.com>
2021-05-07 22:50:43 -0500

    Quiz corrections

--- frontend/src/components/Leader.js
+++ frontend/src/components/Leader.js
@@ -1,18 +1,40 @@
+import { useContext } from 'react';
 import { Card } from 'react-bootstrap';
-import '../styles/Home.css';
+import { LanguageContext } from '../contexts/LanguageContext';
 
 const Leader = ({person}) => {
+  let {isEnglish} = useContext(LanguageContext);
+
   return (
     <div>
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>{person}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Some text</Card.Subtitle>
-          <Card.Text>
-            Stats about {person}
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      {isEnglish
+
+      ?
+
+      <div>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>{person}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Leader TARO</Card.Subtitle>
+            <Card.Text>
+              Stats about {person}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      :
+      <div>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>{person}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Lider TARO</Card.Subtitle>
+            <Card.Text>
+               Estadisticas sobre {person}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      }
     </div>
   );
 };
