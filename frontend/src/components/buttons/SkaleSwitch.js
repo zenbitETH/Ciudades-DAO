52929df9449b38a5de742c9490e20bd4c38d30bf
HabacucV <habacuc.vera@gmail.com>
2021-05-21 07:35:20 -0500

    Skale buttons components and test to not have it hardwired to home

---
+++ frontend/src/components/buttons/SkaleSwitch.js
@@ -0,0 +1,28 @@
+import { Button, Spinner } from 'react-bootstrap';
+import { useContext } from 'react';
+import { LanguageContext } from '../../contexts/LanguageContext';
+import '../../styles/Home.css';
+
+
+const SkaleSwitch = () => {
+  let [isEnglish] = useContext(LanguageContext);
+
+  return (
+    <div>
+    {isEnglish === 'english' ?    
+    <Button variant="secondary" disabled>
+   🧅 Switching to SKALE...
+   <span animation="border"className="spinner-grow" role="status"></span>
+  </Button>
+    :
+    <Button variant="secondary" disabled>
+   🧅 Cambiando a SKALE...
+   <span animation="border"className="spinner-grow" role="status"></span>
+  </Button>
+    }
+    </div>
+
+  );
+};
+
+export default SkaleSwitch;
