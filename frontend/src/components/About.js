c2f31a6c500c14693885cc19e4930af51972572d
HabacucMX <habacuc.vera@gmail.com>
2021-07-30 15:20:32 -0500

    New about page, still on development with focus on Mexican users

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import {Button} from 'react-bootstrap';
 import logo from '../assets/Logow.png';
-import bg1 from '../assets/bg1.png'
+import wallet from '../assets/wallet.gif'
 import bg2 from '../assets/bg2.svg'
 import { LanguageContext } from '../contexts/LanguageContext';
 import ReactPlayer from "react-player";
@@ -15,141 +15,123 @@ const About = () => {
   <div>
     <div class="App">
       <div class="about-ct">
-        <div class="about-txbl">
-          <div class="about-hlbl">About </div>  
+      <div class="about-hl">¿Qué es? </div>  
           <img src={logo} alt="VoTARO" class="logo"/> 
-          <div className="about-tx">Is a voting DApp that rewards users with $TARO when they propose or vote on cities' public needs. It was developed on the Scaling Ethereum Hackathon 2021.</div>
-        </div>  
-        <img src={bg1} alt="VoTARO" class="bg1"/> 
+        <div class="about-tx">
+          <span>VoTARO es una aplicación Web 3.0</span> para votar por propuestas que solucionen una necesidad pública o para organizar una actividad en la ciudad de Querétaro.
+          <br/><br/>
+      </div>  
+        <div class="subt"> Sigue estos pasos para usar tus habilidades digitales en VoTARO y  la web 3. Toca para ver a detalle. </div>
+          <div class="about-us">
+          <a href="#step0" class="us-new"><div class="shake">🏁 <span class="oneem">0. Antes de empezar</span></div><span class="ustext">Conoce más sobre VoTARO y cómo generar valor usando tus habilidades digitales en la web 3</span></a>
+          <a href="#step1" class="us-new"><div class="shake">🔑 <span class="oneem">1. Crear una dirección cripto </span></div><span class="ustext">Manos a la obra, descarga Metamask y crea tu dirección cripto conectarte a la web 3</span></a>
+          <a href="#step2" class="us-mod"><div class="shake">🔐 <span class="oneem">2. Conecta tu dirección</span></div> <span class="ustext">Ahora conecta tu cartera en la pantalla de inicio para que VoTARO identifique tu dirección cripto</span></a>
+          <a href="#step3" class="us-mod"><div class="shake">✔️ <span class="oneem">3. ¿Éres de Querétaro?</span></div><span class="ustext">Valida tu dirección con estas preguntas que solo los Queretan@s sabrán contestar<div class="orange"> ¡Gana hasta 100 TARO!</div></span></a>
+          <a href="#step4" class="us-exp"><div class="shake">💡 <span class="oneem">4. Crea propuestas</span></div><span class="ustext"> Has una propuesta para resolver una necesidad en tu colonia o realizar una actividad en la ciudad<div class="orange"> ¡Gana 20 TARO en las primeras 5 propuestas!</div></span></a>
+          <a href="#step5" class="us-exp"><div class="shake">🗳️ <span class="oneem">5. ¡Vota por las propuestas!</span></div><span class="ustext">¡Es tiempo de votar! Usa el TARO que has ganado para votar por tus propuestas favoritas.<div class="orange"> 1 TARO = 1 Voto</div></span></a>
+        </div>
       </div>
       <div class="about-ct">
-        <div class="about-hlbl">Let's start!</div>  
-        <div class="about-txbl">Depending on your experience using blockchain or ethereum tools, choose if you want to know the basics and what you need to use VoTARO
-        or jump directly to the advanced path to know the dapp details.</div>
-        <div class="about-us">
-          <a href="#new" class="us-new"><div class="shake">🥚</div>New user <span class="ustext">I don't have a Metamask wallet</span></a>
-          <a href="#mod" class="us-mod"><div class="shake">🐣</div>Casual user<span class="ustext">I'm ready for VoTARO</span></a>
-          <a href="#exp" class="us-exp"><div class="shake">🐓</div>Advanced user<span class="ustext">Show me the contract!</span></a>
-        </div>   
+      <div class="about-hl"><a id="step0" class="big-icon">🏁</a><br/> 0. Antes de empezar</div>
+         
+         <div class="ct1-text">
+            VoTARO es una aplicación descentralizada desarrollada con herramientas de la web 3 como Blockchain, Contratos Inteligentes y direcciones cripto. <br/><br/> Si te quedaste así 😵 no te preocupes, continua leyendo para conocer cómo <span class="orange">obtener recompensas por aprender a usar estas herramientas</span> y participar en la gobernanza de la ciudad.
+            Para hacerlo, ocuparás crear una dirección cripto desde una wallet web3. No necesitas dar tus datos personales, <span class="yellow">solo necesitas guardar muy bien la llave privada de tu cuenta,</span> pues solo tu la conocerás y es la única forma de recuperar tu cuenta o usarla en un dispositivo diferente.
+            <br/><br/>
+            <span class="orange"> Si tienes prisa, puedes ir directo a 🔑Crear una dirección cripto</span> o continua leyendo para conocer por qué la web 3 las utiliza en lugar de pedirte tus datos personales para crear una cuenta. 
+           <div class="center"><img src={bg2} alt="Web2 problems" class="web2p"/></div>
+           <br/>
+          La Web 3 es la tercera versión del internet y se concentra en empoderar a los usuarios a través de la criptografía y la verificación del valor;
+           a diferencia de la <span class="yellow">Web 2 que depende de la centralización de servicios y el acceso a tus datos personales para que puedas usar sus aplicaciones.</span>
+           <br/><br/>
+           En cambio, la Web 3 utiliza direcciones cripto que puedes crear sin dar tus datos personales. De esta manera puedes usar aplicaciones como VoTARO de manera segura y sin comprometer tus datos. Desliza hacia abajo para conocer cómo ha evolucionado internet y las principales diferencias entre la Web 2 y la Web 3.
+           <br/><br/>
+        </div>
+       <div class="timeline">
+     <div class="timetl">
+       <div class="timeyr">  Evolución del Internet <div class="orange"> de 1989 a 2020</div></div>
+      
+     </div>
+     <div>
+       <div class="container">
+         <div class="lines">
+           <div class="dot"></div>
+           <div class="line"></div>
+           <div class="dot"></div>
+           <div class="line"></div>
+           <div class="dot"></div>
+         </div>
+         <div class="cards">
+           <div class="web1">
+             <h4 class="">Web 1.0 🔗<div class="timesub"> 1989 - 1998</div></h4>
+             <li class="timetx">Basada en hipervínculos HTML</li>
+             <li class="timetx">Redes Fijas y Unilaterales</li>
+             <li class="timetx">Alcance limitado</li>
+           </div>
+           <div class="web2">
+             <h4 class="">Web 2.0 🌎<div class="timesub"> 1998 - actualidad</div></h4>
+             <li class="timetx">Basada en redes sociales y contenido</li>
+             <li class="timetx">Redes centralizadas para crear y consumir</li>
+             <li class="timetx">Alcance global</li>
+           </div>
+           <div class="web3">
+             <h4 class="">Web 3.0 ☀️<div class="timesub"> 2010 - actualidad</div></h4>
+             <li class="timetx">Basada en contextos y consenso</li>
+             <li class="timetx">Redes descentralizadas para verificar y presevar</li>
+             <li class="timetx">Alcance interplanetario</li>
+           </div>
+         </div>
+       </div>
+       <div class="timetl">
+         <div>Toca las caracteristicas para conocer las diferencias entre:</div>
+       </div>
+       <div class="webvs">
+         <div class="yellow">Web 2.0</div>
+         <div class="orange">Web 3.0</div>
+         <div class="web2vs">
+           <div class="vs">🤴 Centralizado <span class="ustext">Depende de un intermediario.</span></div>
+           <div class="vs">👁️ Invasivo <span class="ustext">Das tus datos personales para usarlo.</span></div>
+           <div class="vs">🔒 Restrictivo <span class="ustext">Solo (algunas) personas lo pueden usar.</span></div>
+           <div class="vs">🚫 Censurable <span class="ustext">Cancela posturas e impone condiciones.</span></div>
+           <div class="vs">📊 Mercado limitado <span class="ustext">Número de usuarios limitado a mercados.</span></div>
+         </div>
+         <div class="web3vs">
+           <div class="vs">🌐 Descentralizado <span class="ustext">Depende de los usuarios.</span></div>
+           <div class="vs">🔑 No invasivos <span class="ustext"> Creas una dirección cripto para usarlo.</span></div>
+           <div class="vs">🔓 Sin restricciones <span class="ustext">Cualquier persona y máquinas lo pueden usar.</span></div>
+           <div class="vs">⛰️ Incensurable <span class="ustext">Preprogramado para  generar consenso.</span></div>
+           <div class="vs">🚀 Sin fronteras <span class="ustext">Número de usuarios definido por contextos.</span></div>
+         </div>
+       </div>
+     </div>
+     </div> 
+       
+       
       </div>
     </div>
     <div class="App">
-        <div class="about-ct1">
-          <div class="about-hl"><a id="new" class="big-icon">🥚</a><br/>New user</div>
-          <div class="ct1-text">
-            Bienvenid@ a VoTARO, seguro tienes muchas dudas sobre el impacto que tiene blockchain en nuestras vidas y te resulta dificil identificar
-            la utilidad de <span class="orange">digitalizar la gobernanza de una ciudad en un registro público, transparente, seguro e inmutable.</span>
-            <br/><br/>
-              Para resolver todas esas dudas, primero viajaremos en el tiempo y entenderemos porque la <span class="orange">Web 3.0 es un esfuerzo global para resolver
-              las problemáticas actuales del internet.</span> 
-            <div class="center"><img src={bg2} alt="Web2 problems" class="web2p"/></div>
-              <br/>
-            Como su nombre lo indica, la Web 3.0 es la tercera versión del internet y se concentra en empoderar a los usuarios a través de la criptografía y la verificación del valor;
-            a diferencia de la <span class="yellow">Web 2.0 que depende de la centralización de servicios</span> donde los beneficios de estas empresas sobre la recopilación de datos de usuarios ha justificado la continuidad de sus fallas. La centralización de nuestros datos y el delegarle la responsabilidad de su uso a un intermediario genera estos problemas:
-          <div class="new-list">
-            <li>Punto singular de falla.</li>
-            <li>Costos por intermediarios "de confianza".</li>
-            <li>Vulnerabilidades de seguridad cibernética.</li>
-            <li>Robo de datos personales.</li>
-            <li>Robo de fondos bancarios.</li>
-          </div>
-         </div>
-        <div class="timeline">
-      <div class="timetl">
-        <h3>Evolución del Internet</h3>
-        <label> de 1989 a 2020</label>
-      </div>
-      <div>
-        <div class="container">
-          <div class="lines">
-            <div class="dot"></div>
-            <div class="line"></div>
-            <div class="dot"></div>
-            <div class="line"></div>
-            <div class="dot"></div>
-            <div class="line"></div>
-            <div class="dot"></div>
-          </div>
-          <div class="cards">
-            <div class="web1">
-              <h4 class="">Web 1.0 🔗<div class="timesub"> 1989 - 1998</div></h4>
-              <li class="timetx">Basada en hipervínculos HTML</li>
-              <li class="timetx">Redes Fijas y Unilaterales</li>
-              <li class="timetx">Alcance limitado</li>
-            </div>
-            <div class="web2">
-              <h4 class="">Web 2.0 🌎<div class="timesub"> 1998 - actualidad</div></h4>
-              <li class="timetx">Basada en redes sociales y contenido</li>
-              <li class="timetx">Redes centralizadas para crear y consumir</li>
-              <li class="timetx">Alcance global</li>
-            </div>
-            <div class="web3">
-              <h4 class="">Web 3.0 ☀️<div class="timesub"> 2010 - actualidad</div></h4>
-              <li class="timetx">Basada en contextos y consenso</li>
-              <li class="timetx">Redes descentralizadas para verificar y presevar</li>
-              <li class="timetx">Alcance interplanetario</li>
-            </div>
-            <div class="web4">
-              <h4>? 🌌<div class="timesub">20XX - ? </div></h4>
-              <li class="timetx">Basada en tiempo y espacio</li>
-              <li class="timetx">Redes cuánticas</li>
-              <li class="timetx">Infraestructura con alcance desconocido</li>
-            </div>
-          </div>
-        </div>
-        <div class="timetl">
-          <div>Toca las caracteristicas para conocer las diferencias entre:</div>
-        </div>
-        <div class="webvs">
-          <div class="yellow">Web 2.0</div>
-          <div class="orange">Web 3.0</div>
-          <div class="web2vs">
-            <div class="vs">🤴 Centralizado</div>
-            <div class="vs">👁️ Invasivo</div>
-            <div class="vs">🔒 Restrictivo</div>
-            <div class="vs">🚫 Censurable</div>
-            <div class="vs">📊 Mercado limitado</div>
-            <div class="vs">👎 Navegas con tus datos</div>
-          </div>
-          <div class="web3vs">
-            <div class="vs">🌐 Descentralizado</div>
-            <div class="vs">🙅‍♀️ No invasivos</div>
-            <div class="vs">🔓 Sin restricciones</div>
-            <div class="vs">⛰️ Incensurable</div>
-            <div class="vs">🚀 Sin fronteras</div>
-            <div class="vs">👍 Navegas con una wallet</div>
-          </div>
-        </div>
-      </div>
-      </div>
-        <div class="aboutwr">
-          <div class="big-icon">⚠️</div>
-          <h3>Atención</h3>
-          <div class="ct1-text"><span class="yellow">VoTARO es una aplicación de la Web 3.0</span>, por lo tanto <span class="orange">necesitas una wallet descentralizada</span> para interactuar. 
+        <div class="about-ct">
+        <div class="about-hl"><a id="step1" class="big-icon">🔑</a><br/> 1. Crear dirección cripto</div>
+        <div class="about-tx">
+        En VoTARO tendrás que usar tus habilidades digitales para participar y obtener recompensas del token TARO, fichas digitales que equivalen a 1 voto a favor o en contra de tus propuestas preferidas.
           <br/><br/>
-          Para obtenerla no necesitas dar tus datos personales, solo necesitas <span class="yellow">guardar muy bien la llave privada de tu cuenta</span>, pues <span class="red">solo tu la conocerás y 
+          Para participar ocuparás crear una dirección cripto desde una wallet web3. No necesitas dar tus datos personales, solo necesitas <span class="orange">guardar muy bien la llave privada de tu cuenta</span>, pues <span class="red">solo tu la conocerás y 
           es la única forma de recuperar tu cuenta</span> o usarla en un dispositivo diferente.
-          </div>
-          <Button class="" > <a href="https://metamask.io">🦊 Descargar Cartera</a></Button>          
-        </div>    
-
+          <br/><br/>
+          <div class="Wallet"><Button ><a href="https://metamask.io">🔑 Descargar wallet web3</a></Button></div>
+          <br/>
+          <div class="orange">Aquí un tutorial del paso a paso para descargarla desde un navegador web.<br/><br/><ReactPlayer width="100%"  url="https://youtu.be/Abzogd_3VBA"/></div>
         </div>
-        <div class="about-ct2">
-          <div class="about-hlbl"><a id="mod" class="big-icon">🐣</a><br/>Casual User</div>
-          <div className="text-medium-left">
-            <div className="purple">✋ Propón</div>
-            <div className="purple">🗳️ Vota</div>
-            <div className="purple">🥇 Obtén $TARO</div>
-          </div>    
         </div>
-        <div class="about-ct3">
-          <div class="about-hlbl"><a id="exp" class="big-icon">🐓</a><br/>Advanced User</div>
-          <div className="text-medium-left">
-            <div className="purple">Ropsten contract</div>
-            <div className="purple">Tokenomics</div>
-            <div className="purple">Github </div>
-          </div>    
+        <div class="about-ct1">
+          <div class="about-hlbl"><a id="step2" class="big-icon">🔐</a><br/>2. Conecta tu dirección</div>
+          <div class="about-txbl">¡Ya tienes dirección cripto! Ahora usala para conectarte a VoTARO. Regresa <a class="alt" href="/Home">aquí</a> a la pantalla principal y presiona:<br/><br/><div class="center"><Button>🔑Conectar Dirección Cripto</Button></div></div>
+          <br/>
+          <div class="about-txbl">Tras presionarlo aparecerá una ventana parcecida a la de abajo, toca "siguiente" y "Conectar" para establecer conexión con VoTARO.</div>  
+          <img src={wallet} alt="VoTARO" class="walletct"/>
+          
         </div>
+      
       </div>
       
     </div>
