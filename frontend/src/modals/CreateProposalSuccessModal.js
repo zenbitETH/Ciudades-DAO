9a2aca934853f497547e973daf2d7aeb109646ff
HabacucMX <habacuc.vera@gmail.com>
2022-07-12 22:43:21 -0500

    Create proposal update

--- frontend/src/modals/CreateProposalSuccessModal.js
+++ frontend/src/modals/CreateProposalSuccessModal.js
@@ -12,6 +12,39 @@ const CreateProposalSuccessModal = (props) => {
   return (
     <div >
       {isEnglish === 'english' ?
+     <Modal
+     class="grid-block"
+     {...props}
+     size="xl"
+     aria-labelledby="contained-modal-title-vcenter"
+     centered
+     className="main"
+   >
+   <div class="center"><div class="jump"><img src={send} alt="Alert about verification" class="prop-img"/></div></div>
+   <h1><div >Proposal sended! </div></h1><br/>
+     
+     <h3>
+        Your proposal is ready to be voted, <span class="yellow">use your VOTO to vote.</span>
+       <span class="yellow"> The first 20 proposals will give you 50 VOTO each! </span>
+       Create more proposals to increase your voting power. 
+     </h3>
+
+     <div class="void-link">
+       <div class="prop-bg2"><a href="/ProposalList#vote">
+           <img src={vote2} class="ribvan"/> 
+           <div class="propsub">Avaliable proposals</div>
+           <div class="propopt">Vote</div>
+         </a></div>
+         <div class="prop-bg2"><a href="/CreateProposal">
+           <img src={prop} class="ribvan"/>
+           <div class="propsub">New proposal</div>
+           <div class="propopt">Propose </div>
+         </a></div>
+       </div>
+     
+   </Modal>
+      
+      :
       <Modal
       class="grid-block"
       {...props}
@@ -23,8 +56,7 @@ const CreateProposalSuccessModal = (props) => {
     <div class="center"><div class="jump"><img src={send} alt="Alert about verification" class="prop-img"/></div></div>
     <h1><div >¡Propuesta enviada! </div></h1><br/>
     <h3>Tu propuesta esta lista para ser votada<span class="yellow"> usa tu VOTO para votar. </span>
-      <span class="yellow">¡Las primeras 20 propuestas te darán 50 VOTO cada una! </span>
-      Crea mas propuestas para incrementar tu poder de voto. 
+
     </h3>
       <div class="void-link">
         <div class="prop-bg2"><a href="/ProposalList#vote">
@@ -41,38 +73,6 @@ const CreateProposalSuccessModal = (props) => {
       
     </Modal>
       
-      :
-      <Modal
-        class="grid-block"
-        {...props}
-        size="xl"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        className="main"
-      >
-      <div class="center"><div class="jump"><img src={send} alt="Alert about verification" class="prop-img"/></div></div>
-      <h1><div >Proposal sended! </div></h1><br/>
-        
-        <h3>
-           Your proposal is ready to be voted, <span class="yellow">use your VOTO to vote.</span>
-          <span class="yellow"> The first 20 proposals will give you 50 VOTO each! </span>
-          Create more proposals to increase your voting power. 
-        </h3>
-
-        <div class="void-link">
-          <div class="prop-bg2"><a href="/ProposalList#vote">
-              <img src={vote2} class="ribvan"/> 
-              <div class="propsub">Avaliable proposals</div>
-              <div class="propopt">Vote</div>
-            </a></div>
-            <div class="prop-bg2"><a href="/CreateProposal">
-              <img src={prop} class="ribvan"/>
-              <div class="propsub">New proposal</div>
-              <div class="propopt">Propose </div>
-            </a></div>
-          </div>
-        
-      </Modal>
     }
     </div>
   );
