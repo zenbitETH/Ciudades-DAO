3e0c052f3b4beeda2ef1b23ecc8fb91a39d786f2
HabacucMX <habacuc.vera@gmail.com>
2022-08-17 15:38:41 -0500

    Corrección en modal de cuestionario satisfactorio

--- frontend/src/modals/QuizSuccessModal.js
+++ frontend/src/modals/QuizSuccessModal.js
@@ -16,19 +16,23 @@ const QuizSuccessModal = (props) => {
         <h1><span id="vote" class="yellow-jos">¡Felicidades! </span></h1><br/>
           <div class="center"><div class="jump"><img src={reward} alt="Alert about verification" class="prop-img"/></div></div>
           <h1><span> Ahora tienes VOTO</span></h1>
-          <h3> <span class="yellow">Has validado cuenta por seis meses y tu recompensa en tokens VOTO se han transferido a su cuenta. </span></h3>
+          <h3> <span class="yellow">Se ha depositado tokens VOTO en tu cuenta y has obtenido la Certificación Web3 de Gobernador Alfa, la cual es válida por seis meses. </span></h3>
         <Modal.Body>
         <div class="void-link">
-              <div class="prop-bgr"><a href="/createProposal">
-                <img src={prop} class="ribvan"/> 
-                <div class="propsub">Crea una propuesta</div>
-                <div class="propopt">Proponer</div>
-              </a></div>
-              <div class="prop-bgr"><a href="/ProposalList#vote">
-              <img src={vote2} class="ribvan"/> 
-              <div class="propsub">Propuestas</div>
-              <div class="propopt">Por votar</div>
-            </a></div>
+              <div class="prop-bgr">
+                <a href="/createProposal">
+                  <img src={prop} class="ribvan"/> 
+                  <div class="propsub">Crea una propuesta</div>
+                  <div class="propopt">Proponer</div>
+                </a>
+              </div>
+              <div class="prop-bgr">
+                <a href="/ProposalList#vote">
+                  <img src={vote2} class="ribvan"/> 
+                  <div class="propsub">Propuestas</div>
+                  <div class="propopt">Por votar</div>
+                </a>
+              </div>
             </div>
         </Modal.Body>
       </Modal>
